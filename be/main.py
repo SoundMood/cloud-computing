5f9a879c965b82772b79de5152612a0fd9a6aad9
k124k3n <k124k3n@gmail.com>
2024-12-07 18:56:37 +0700

    Add multi-thread for Pub/Sub consume

--- be/main.py
+++ be/main.py
@@ -1,6 +1,13 @@
 import uvicorn
+import threading
+import asyncio
 from app.controller import listen_to_pubsub
-# TODO: Migrate service account to current at k124k3n
+
+def start_pubsub_listener():
+    asyncio.run(listen_to_pubsub())
+
 if __name__ == "__main__":
-    listen_to_pubsub()
-    uvicorn.run("app:views", host="0.0.0.0", port=8081, reload=True)
+    # Run the Pub/Sub listener in a separate thread
+    listener_thread = threading.Thread(target=start_pubsub_listener)
+    listener_thread.start()
+    uvicorn.run("app:views", host="0.0.0.0", port=80, reload=True)
