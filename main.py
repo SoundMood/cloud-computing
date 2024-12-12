1f66ae2a6d384e7b4fd61202f83bf48a8585c3d1
k124k3n <k124k3n@gmail.com>
2024-12-12 08:49:33 +0700

    Imple pub/sub push

--- main.py
+++ main.py
@@ -1,39 +1,11 @@
-# import uvicorn
-# import threading
-# import asyncio
-# from fastapi import FastAPI
-# from pubsub import listen_to_pubsub
-
-# app = FastAPI()
-
-# # Create a threading event to signal the listener thread to stop
-# stop_event = threading.Event()
-
-# async def listen_to_pubsub_with_stop():
-#     while not stop_event.is_set():
-#         await listen_to_pubsub()
-
-# def start_pubsub_listener():
-#     asyncio.run(listen_to_pubsub_with_stop())
-
-# if __name__ == "__main__":
-#     # Run the Pub/Sub listener in a separate thread
-#     listener_thread = threading.Thread(target=start_pubsub_listener)
-#     listener_thread.start()
-
-#     try:
-#         # Run the FastAPI application
-#         uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
-#     except KeyboardInterrupt:
-#         # Signal the listener thread to stop
-#         stop_event.set()
-#         listener_thread.join()
-
 from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
+from pubsub import publish_message
+from predict import predict
 import json
 import uvicorn
 import base64
+
 app = FastAPI()
 
 class PubSubMessage(BaseModel):
@@ -44,22 +16,16 @@ class PubSubMessage(BaseModel):
 async def pubsub_push(request: Request):
     try:
         body = await request.json()
-        print(body)
         pubsub_message = PubSubMessage(**body)
         message_data = pubsub_message.message.get("data")
         attributes = pubsub_message.message.get("attributes", {})
         message_id = attributes.get("id")
         message_data = base64.b64decode(message_data).decode('utf-8')        
+        decoded_message = json.loads(message_data)
         print(f"Attributes: {attributes}")
         print(f"Message ID: {message_id}")
-        
-        if message_data:
-            decoded_message = json.loads(message_data)
-            print("HI ", decoded_message['image_name'])
-            print(f"Received message: {decoded_message}")
-            
-        else:
-            print("Received message with no data.")
+        publish_message(message_id, predict(decoded_message['image_name'], decoded_message['access_token']))
+
         return {"status": "success"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error processing Pub/Sub message: {e}")
--- pubsub.py
+++ pubsub.py
@@ -3,9 +3,6 @@ import json
 import settings
 from predict import predict
 
-subscriber = pubsub_v1.SubscriberClient()
-subscription_path = subscriber.subscription_path(settings.PROJECT_ID, 'get-predict')
-
 def publish_message(id, message_data):
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(settings.PROJECT_ID, settings.TOPIC_NAME)
@@ -13,21 +10,6 @@ def publish_message(id, message_data):
     message_json = json.dumps(message_data)
     message_bytes = message_json.encode('utf-8')
 
-    
     future = publisher.publish(topic_path, message_bytes, id=str(id), type='plz-predict')
-
-
-async def listen_to_pubsub():
-    def callback(message):
-        print(message.data.decode('utf-8'))
-        message_data = json.loads(message.data.decode('utf-8'))
-        publish_message(message.attributes['id'], predict(message_data['image_name'], message_data['access_token']))
-        message.ack()
-
-    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-    try:
-        streaming_pull_future.result()
-    except Exception as e:
-        streaming_pull_future.cancel()
-        raise e
+    print(f"Published message ID {future.result()}")
     
