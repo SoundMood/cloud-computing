7d5d216cff011295156498aedfec63dad5fed594
k124k3n <k124k3n@gmail.com>
2024-12-12 07:59:18 +0700

    Pub/sub test

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ async def pubsub_push(request: Request):
         
         if message_data:
             decoded_message = json.loads(message_data)
+            print("HI ", message_data['image_name'])
             print(f"Received message: {decoded_message}")
             
         else:
